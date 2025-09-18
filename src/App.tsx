16ea27316cf8e4f5fc2b21093dd9930dae2a66f0
maryam-kpmpower <maryam@kpmpower.com>
2025-09-18 16:56:07 -0400

    prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import Login from './components/Login/Login.tsx';
 // import './App.css'
 
 function App() {
-
   return (
     <div>
       <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,44 +4,56 @@ import { validateLogin } from '../LoginUtils';
 import '../../scss/Login.scss';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
-        // stops the page from refreshing when form is submitted
-        e.preventDefault();
+  const handleSubmit = (e: React.FormEvent) => {
+    // stops the page from refreshing when form is submitted
+    e.preventDefault();
 
-        if (!validateLogin(username, password, users)) {
-            setError('Invalid username or password.');
-            return
-        }
-
-        setError('');
-        alert('Welcome!')
+    if (!validateLogin(username, password, users)) {
+      setError('Invalid username or password.');
+      return;
     }
 
-    return (
-        <div className='login-page'>
-            <h2 className='login-title'>Login Page</h2>
-            <form className='login-form' onSubmit={handleSubmit}>
-                <label htmlFor='username' className='username-label'>
-                    <span>Username</span>
-                    <input className='username-input-field' id='username' type='text' value={username} onChange={(e) => setUsername(e.target.value)}/>
-                </label>
-                <label htmlFor='password' className='password-label'>
-                    <span>Password</span>
-                    <input className='password-input-field' id='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </label>
-                <div className='login-action'>
-                    <button className='login-btn' type='submit'>Login</button>
-                </div>
-                <p className='login-error'>{error || " "}</p>
-            </form>
+    setError('');
+    alert('Welcome!');
+  };
+
+  return (
+    <div className="login-page">
+      <h2 className="login-title">Login Page</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="username" className="username-label">
+          <span>Username</span>
+          <input
+            className="username-input-field"
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password" className="password-label">
+          <span>Password</span>
+          <input
+            className="password-input-field"
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div className="login-action">
+          <button className="login-btn" type="submit">
+            Login
+          </button>
         </div>
-    )
-
-
-}
+        <p className="login-error">{error || ' '}</p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
